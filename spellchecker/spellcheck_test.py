c9cbe8a462e081ff63c22f7ebeb1816d0a2ca8a4
Mutiu Jimoh-Akindele <10276658@mydbs.ie>
2017-06-29 12:12:53 +0100

    handle failed words

--- spellchecker/spellcheck_test.py
+++ spellchecker/spellcheck_test.py
@@ -7,15 +7,28 @@ class TestSpellChecker(unittest.TestCase):
         self.spellchecker.load_words('spell.words')
 
     def test_spell_checker(self):
-        self.assertTrue(self.spellchecker.check_word('zygotic'))
-        self.assertFalse(self.spellchecker.check_words('zygotic mistasdas elementary'))
-        self.assertTrue(self.spellchecker.check_words('our first correct sentence'))
-		# handle case sensitivity 
-        self.assertTrue(self.spellchecker.check_words('Our first correct sentence'))
+		self.assertTrue(self.spellchecker.check_word('zygotic'))
+		failed_words = self.spellchecker.check_words('zygotic mistasdas elementary')
+		self.assertEquals(1, len(failed_words))
+		self.assertEquals('mistasdas', failed_words[0])
+		self.assertEquals(0, len(self.spellchecker.check_words('our first correct sentence')))
+        # handle case sensitivity
+		self.assertEquals(0, len(self.spellchecker.check_words('Our first correct sentence')))
         # handle full stop
-        self.assertTrue(self.spellchecker.check_words('Our first correct sentence.'))
+		self.assertEquals(0, len(self.spellchecker.check_words('Our first correct sentence.')))
+		failed_words = self.spellchecker.check_words('zygotic mistasdas spelllleeeing elementary')
+		self.assertEquals(2, len(failed_words))
+		self.assertEquals('mistasdas', failed_words[0])
+		self.assertEquals('spelllleeeing', failed_words[1])
+
+        #self.assertFalse(self.spellchecker.check_words('zygotic mistasdas elementary'))
+        #self.assertTrue(self.spellchecker.check_words('our first correct sentence'))
+		# handle case sensitivity 
+        #self.assertTrue(self.spellchecker.check_words('Our first correct sentence'))
+        # handle full stop
+        #self.assertTrue(self.spellchecker.check_words('Our first correct sentence.'))
 		#to fix the errors, the code on newspell was modified on line 10--returning word.strip('.').lower() in self.words instead of word in self.words  
-        self.assertFalse(self.spellChecker.check_words('zygotic mistasdas spelllleeeing elementary'))
+        #self.assertFalse(self.spellchecker.check_words('zygotic mistasdas spelllleeeing elementary'))
 
 if __name__ == '__main__':
     unittest.main()
